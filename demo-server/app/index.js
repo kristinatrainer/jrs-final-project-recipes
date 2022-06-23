fddf7a6cd3c214066e9744a05789296234d87b53
kristinatrainer <100874466+kristinatrainer@users.noreply.github.com>
2022-06-23 08:37:36 -0400

    connect to DB via Mongoose

--- demo-server/app/index.js
+++ demo-server/app/index.js
@@ -1,18 +1,30 @@
-// connect to mySQL
-const mysql = require('mysql');
-// add config
+// connect to mongoDB
 const dbConfig = require('./config/db.config');
 
-const connection = mysql.createConnection(dbConfig);
+const mongoose = require("mongoose");
 
-connection.connect((err) => {
-    if (err) {
-        throw err;
-    } else {
-        console.log("Connected to mySQL database: " + dbConfig.database)
+mongoose.Promise = global.Promise;
 
-    }
-})
+const db = {};
+db.mongoose = mongoose;
+db.url = dbConfig.url;
 
-// export the connection
-module.exports = connection; 
+db.meals = require("./models/meals.model.js")(mongoose);
+
+db.mongoose
+    .connect(db.url, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+         })
+    .then(() => {
+        console.log("Connected to the database!");
+    })
+    .catch(err => {
+        console.log("Cannot connect to the database!", err);
+        process.exit();
+    });
+
+module.exports = db;
+
+
+
